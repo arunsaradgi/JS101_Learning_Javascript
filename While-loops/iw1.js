30fca82d53814e406f4436045e39ce7acd53f194
Arun Saradgi <saradgi07@gmail.com>
2022-10-13 21:23:07 +0530

    adding while loops

---
+++ While-loops/iw1.js
@@ -0,0 +1,9 @@
+//Print the numbers from the given starting point till ending point (including both start and end)
+
+let num = 1;
+let end = 10;
+
+while (num <= end) {
+  console.log(num);
+  num++;
+}
